8c25a9192aa7aaa106816111483f3e58fe8589ac
The-Ishikawa <96249544+The-Ishikawa@users.noreply.github.com>
2022-05-17 19:10:59 +0900

    miner change

--- lib/Provider_SongDataProvider.dart
+++ lib/Provider_SongDataProvider.dart
@@ -2,27 +2,27 @@ import 'package:flutter/material.dart';
 
 class SongDataProvider extends ChangeNotifier{
   Map _songDataMap = {};
-  //List _idList = [];
+  List _idList = [];
 
   Map<dynamic, dynamic> get songDataMap => _songDataMap;
-  //List get idList => _idList;
+  List get idList => _idList;
 
   void setSongDataMap(Map _path){
     _songDataMap = _path;
-    //mapToList();
+    mapToList();
     notifyListeners();
   }
 
-  /*
+
   void mapToList(){
     int _i = 0;
-    while(true){
-      _idList.add(_songDataMap[_i]["id"]);
-      if(_songDataMap[_i]['notes'] == -1){
+    while(true) {
+      _idList.add(_songDataMap["IIdx"][_i]["id"]);
+      if (_songDataMap["IIdx"][_i]['notes'] == -1) {
         break;
       }
       _i++;
     }
   }
-  */
+
 }
